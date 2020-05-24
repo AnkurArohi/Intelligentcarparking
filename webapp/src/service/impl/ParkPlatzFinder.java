c6475294819d1239a62ddc88bf12a41b0192f762
Ankurarohi <ankur.arohi@rwth-aachen.de>
2020-05-24 09:55:31 +0200

    Compile error removed

---
+++ webapp/src/service/impl/ParkPlatzFinder.java
@@ -0,0 +1,6 @@
+import java.util.List;
+import java.util.Map;
+public interface ParkPlatzFinder {
+//INFO TO uniquely identify a place both PLZ and the strasse name are required
+    Map<String, Strasse> getParkPlatz(Strasse dest, Double radius);
+}
--- webapp/src/service/impl/ParkplatzFinder.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-//INFO TO uniquely identify a place both PLZ and the strasse name are required
-
-public interface ParkPlatzFinder{
-
-    Map<Str, Strasse> getParkPlatz(Strasse dest, Double radius);
-
-}
