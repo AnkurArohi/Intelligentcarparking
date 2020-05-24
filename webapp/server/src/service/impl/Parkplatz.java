6f9d391a62c8519b1d9c8dddfb4c2bf08522a4a3
Ankurarohi <ankur.arohi@rwth-aachen.de>
2020-05-24 11:23:04 +0200

    Parkplatz class corr

--- webapp/server/src/service/impl/Parkplatz.java
+++ webapp/server/src/service/impl/Parkplatz.java
@@ -7,22 +7,23 @@ public class Parkplatz extends Strasse{
 
     public Parkplatz(String addr, Integer plz, Integer capacity) {
         super(addr, plz);
-        this.name = getName();
-        this.capacity =capacity;
+        this.name = super.getName();
+        this.setCapacity(capacity);
     }
     public Double getCost() {
         return cost;
     }
-
     public void setCost(Double cost) {
         this.cost = cost;
     }
     public Integer getCapacity() {
         return capacity;
     }
-    public String getName(){
-        return name;
+    private void setCapacity(Integer capacity) {
+        this.capacity = capacity;
     }
+    public String getName(){
+        return name;}
     public Double getProbability() {
         return probability;
     }
